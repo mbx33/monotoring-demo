2e6909d75e38a3a123932207223d2d327935ce18
Matt Baxter <mbx33@hotmail.com>
2021-08-17 09:56:43 -0600

    added something

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const app = express();
 
 const path = require('path');
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '64ca2fa6908349899e2e272bce60c1e6',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 app.use(express.json());
 
 const port = process.env.PORT || 4040;
 
 app.get('/', function(req, res) {
+    rollbar.log('App.get homepage hit')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
